68fb0a2fa60700a8c88f703fa664ae5ca2a5b7e0
Joachim Bargsten <jw@bargsten.org>
2022-11-18 10:36:56 +0100

    added more tests

--- tests/test_core_modules.py
+++ tests/test_core_modules.py
@@ -1,6 +1,17 @@
+import pytest
 import py3arch.core_modules as cm
 
 
-def test_read():
+def test_read_legacy():
     modules = cm.list_core_modules([3, 9])
     assert "__future__" in modules
+
+
+def test_read():
+    modules = cm.list_core_modules([3, 10])
+    assert "__future__" in modules
+
+
+def test_read_error():
+    with pytest.raises(FileNotFoundError):
+        cm.list_core_modules([3, 5])
