c8386b41827058b0f7e5365fe7f23e34d59d4523
Joachim Bargsten <jw@bargsten.org>
2022-11-18 14:28:57 +0100

    fixed core module list tests

--- tests/test_core_modules.py
+++ tests/test_core_modules.py
@@ -3,15 +3,15 @@ import py3arch.core_modules as cm
 
 
 def test_read_legacy():
-    modules = cm.list_core_modules([3, 9])
+    modules = cm.list_core_modules((3, 9))
     assert "__future__" in modules
 
 
 def test_read():
-    modules = cm.list_core_modules([3, 10])
+    modules = cm.list_core_modules((3, 10))
     assert "__future__" in modules
 
 
 def test_read_error():
     with pytest.raises(FileNotFoundError):
-        cm.list_core_modules([3, 5])
+        cm.list_core_modules((3, 5))
