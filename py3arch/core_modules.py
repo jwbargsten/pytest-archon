de7740e5479f83270c7d912289a5dc30adfec576
Joachim Bargsten <jw@bargsten.org>
2022-11-18 10:32:48 +0100

    fmt

--- py3arch/core_modules.py
+++ py3arch/core_modules.py
@@ -5,16 +5,19 @@ from pathlib import Path
 
 
 def list_core_modules(version=None):
-  if version is None:
-    version = [sys.version_info.major, sys.version_info.major]
-  if version[0] == 3 and version[1] > 9:
-    modules = list(
-      set(list(sys.stdlib_module_names) + list(sys.builtin_module_names)))
-  else:
-    modules_file = Path(
-      pkg_resources.resource_filename("py3arch", str(Path("assets", "core-module-lists", f"{version[0]}.{version[1]}.txt"))))
-    if not modules_file.exists():
-      raise FileNotFoundError(
-        f"{modules_file} does not exist, perhaps your python version is not supported")
-    modules = modules_file.read_text().splitlines()
-  return modules
+    if version is None:
+        version = [sys.version_info.major, sys.version_info.major]
+    if version[0] == 3 and version[1] > 9:
+        modules = list(set(list(sys.stdlib_module_names) + list(sys.builtin_module_names)))
+    else:
+        modules_file = Path(
+            pkg_resources.resource_filename(
+                "py3arch", str(Path("assets", "core-module-lists", f"{version[0]}.{version[1]}.txt"))
+            )
+        )
+        if not modules_file.exists():
+            raise FileNotFoundError(
+                f"{modules_file} does not exist, perhaps your python version is not supported"
+            )
+        modules = modules_file.read_text().splitlines()
+    return modules
--- tests/test_core_modules.py
+++ tests/test_core_modules.py
@@ -1,6 +1,6 @@
 import py3arch.core_modules as cm
 
-def test_read():
-  modules = cm.list_core_modules([3, 9])
-  assert "__future__" in modules
 
+def test_read():
+    modules = cm.list_core_modules([3, 9])
+    assert "__future__" in modules
