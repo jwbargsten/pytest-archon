b12c4eb8e97276673f5d09fc6d8e5bed60c5feba
Arjan Molenaar <gaphor@gmail.com>
2022-11-18 11:57:04 +0100

    Fix import of system modules

--- py3arch/collect.py
+++ py3arch/collect.py
@@ -1,3 +1,4 @@
+import sys
 from pathlib import Path
 from typing import Iterable
 from modulefinder import ModuleFinder, Module
@@ -5,7 +6,7 @@ from modulefinder import ModuleFinder, Module
 
 def collect_modules(base_path: Path) -> Iterable[tuple[tuple[str], Path, Module]]:
     for module_path in base_path.glob('**/*.py'):
-        finder = ModuleFinder(path=[str(base_path)])
+        finder = ModuleFinder(path=[str(base_path), *sys.path])
         finder.run_script(str(module_path))
 
         yield (path_to_module_name(module_path, base_path), module_path, list(finder.modules.values()))
--- tests/test_collect.py
+++ tests/test_collect.py
@@ -20,24 +20,50 @@ def create_testset(tmp_path):
     return _create_testset
 
 
-def xtest_collect_modules(create_testset):
+def test_collect_modules(create_testset):
 
     path = create_testset(("mymodule.py", ""))
 
-    collected = list(collect_modules(path))
+    collected = list(path.name for name, path, mod in collect_modules(path))
 
     assert "mymodule.py" in collected
+
+
+def test_collect_with_system_modules(create_testset):
+
+    path = create_testset(("mymodule.py", "import sys, os"))
+
+    name, path, modules = next(collect_modules(path))
+    modules = [m.__name__ for m in modules]
+    
+    assert name == ("mymodule",)
+    assert "sys" in modules
+    assert "os" in modules
 
 
 def test_module_imports_other_module(create_testset):
 
     path = create_testset(
-        ("module.py", "import othermodule"),
+        ("module.py", ""),
         ("othermodule.py", "import module")
     )
 
     module_names = [m.__name__ for name, path, modules in collect_modules(path) for m in modules]
 
     assert "module" in module_names
-    assert "othermodule" in module_names
+    assert "othermodule" not in module_names
+
+
+def test_module_import_nested_modules(create_testset):
+
+    path = create_testset(
+        ("package/__init__.py", ""),
+        ("package/module.py", ""),
+        ("othermodule.py", "import package.module")
+    )
+
+    module_names = [m.__name__ for name, path, modules in collect_modules(path) for m in modules]
+
+    assert "package.module" in module_names
+    assert "othermodule" not in module_names
 
