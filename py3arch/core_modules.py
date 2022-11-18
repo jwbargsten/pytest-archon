23829fdc2e2ea66106ebf861a222fb4ac937cc44
Joachim Bargsten <jw@bargsten.org>
2022-11-18 10:32:17 +0100

    1st test for reading in core modules

--- py3arch/core_modules.py
+++ py3arch/core_modules.py
@@ -6,14 +6,13 @@ from pathlib import Path
 
 def list_core_modules(version=None):
   if version is None:
-    version = sys.version_info
-  if version.major == 3 and version.minor > 9:
+    version = [sys.version_info.major, sys.version_info.major]
+  if version[0] == 3 and version[1] > 9:
     modules = list(
       set(list(sys.stdlib_module_names) + list(sys.builtin_module_names)))
   else:
     modules_file = Path(
-      pkg_resources.resource_filename("assets", "core-module-lists",
-                                      f"{version.major}.{version.minor}"))
+      pkg_resources.resource_filename("py3arch", str(Path("assets", "core-module-lists", f"{version[0]}.{version[1]}.txt"))))
     if not modules_file.exists():
       raise FileNotFoundError(
         f"{modules_file} does not exist, perhaps your python version is not supported")
