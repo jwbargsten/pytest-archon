3339558ca4ff5f7ed2f96f13c13f344862929f34
Arjan Molenaar <gaphor@gmail.com>
2022-11-18 14:43:57 +0100

    Add test for from- and relative imports

--- tests/test_collect.py
+++ tests/test_collect.py
@@ -1,6 +1,3 @@
-from glob import iglob
-from modulefinder import ModuleFinder
-
 from textwrap import dedent
 
 import pytest
@@ -51,6 +48,15 @@ def test_module_imports_other_module(create_testset):
     assert "othermodule" not in module_names
 
 
+def test_module_import_from(create_testset):
+
+    path = create_testset(("module.py", "val = 1"), ("othermodule.py", "from module import val"))
+
+    module_names = {m.__name__ for name, path, modules in collect_modules(path) for m in modules}
+
+    assert "module" in module_names
+    assert "othermodule" not in module_names
+
 def test_module_import_nested_modules(create_testset):
 
     path = create_testset(
@@ -61,3 +67,20 @@ def test_module_import_nested_modules(create_testset):
 
     assert "package.module" in module_names
     assert "othermodule" not in module_names
+
+
+@pytest.mark.xfail
+def test_relative_imports(create_testset):
+
+    path = create_testset(
+        ("package/__init__.py", ""),
+        ("package/module.py", "from .importme import val"),
+        ("package/importme.py", "val = 1"),
+        ("package/sub/__init__.py", ""),
+        ("package/sub/deep.py", "from ..importme import val"),
+    )
+
+    print(list(collect_modules(path)))
+    module_names = {m.__name__ for name, path, modules in collect_modules(path) for m in modules}
+
+    assert "package.importme" in module_names
