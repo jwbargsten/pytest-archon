c3adbf68150605fd9b9dcd6528afba22dcd3989d
Joachim Bargsten <jw@bargsten.org>
2022-11-20 13:46:41 +0100

    cleaned up the mess

--- py3arch/import_finder.py
+++ py3arch/import_finder.py
@@ -4,25 +4,8 @@ import importlib
 import os
 
 
-# from mdulefinder
 def find_module(name, path=None):
-    """An importlib reimplementation of imp.find_module (for our purposes)."""
-
-    # It's necessary to clear the caches for our Finder first, in case any
-    # modules are being added/deleted/modified at runtime. In particular,
-    # test_modulefinder.py changes file tree contents in a cache-breaking way:
-
     importlib.machinery.PathFinder.invalidate_caches()
-
-    # parent_name = name.rpartition('.')[0]
-    # if parent_name:
-    #     spec = find_module(parent_name, path)
-    #     if spec is None:
-    #         raise ImportError("No module named {name!r}".format(name=name), name=name)
-    #     if not spec.submodule_search_locations:
-    #         raise ImportError("No submodule search locations for {name!r}".format(name=name), name=name)
-    #     print(spec)
-    # else:
     spec = importlib.machinery.PathFinder.find_spec(name, path)
 
     if spec is None:
@@ -31,31 +14,10 @@ def find_module(name, path=None):
     return spec
 
 
-# importlib._bootstrap._resolve_name
-def _resolve_name(name, package, level):
-    """Resolve a relative module name to an absolute one."""
-    bits = package.rsplit('.', level - 1)
-    if len(bits) < level:
-        raise ImportError('attempted relative import beyond top-level package')
-    base = bits[0]
-    return '{}.{}'.format(base, name) if name else base
-
-
-class FileProcessor:
-    # importlib.import_module()
-    pass
-
-
-def build_ast(data):
-    return ast.parse(data)
-
-
-def resolve_import_from(name, module=None, package=None, level=None, path=None):
-    fqname = resolve_import_from1(name, module, package, level, path)
-
+def resolve_module_or_object(fqname, path=None):
     if not fqname.count("."):
         return fqname
-    bits = fqname.split('.')
+    bits = fqname.split(".")
     parent_name = bits[0]
 
     spec = find_module(parent_name, path)
@@ -71,28 +33,16 @@ def resolve_import_from(name, module=None, package=None, level=None, path=None):
         # we imported an object, return "parent"
         return ".".join(bits[:-1])
 
-    #     finder = importlib.machinery.FileFinder(path=base_path)
-    #     spec = finder.find_spec(fqname)
-    #     if spec is None:
-    #         newname = fqname.rpartition('.')[0]
-    #         spec = finder.find_spec(newname)
-    #         if spec is None:
-    #             raise ImportError("No module named {name!r}".format(name=fqname), name=fqname)
-    #         return newname
-    # return fqname
 
-
-def resolve_import_from1(name, module=None, package=None, level=None, path=None):
-    if path is None:
-        path = sys.path
+def resolve_import_from(name, module=None, package=None, level=None):
     if not level:
         # absolute import
         return name if module is None else "{}.{}".format(module, name)
 
     # taken from importlib._bootstrap._resolve_name
-    bits = package.rsplit('.', level - 1)
+    bits = package.rsplit(".", level - 1)
     if len(bits) < level:
-        raise ImportError('attempted relative import beyond top-level package')
+        raise ImportError("attempted relative import beyond top-level package")
     base = bits[0]
 
     # relative import
@@ -104,28 +54,17 @@ def resolve_import_from1(name, module=None, package=None, level=None, path=None)
         return "{}.{}.{}".format(base, module, name)
 
 
-# from . import moduleX
-# if module is None:
-
-# import .moduleX
-# if module is not None:
-
-
-# if module is None:
-# relative import
-# raise ModuleNotFoundError("no module name supplied, cannot resolve relative imports")
-# if not level:
-#     return ".".join([module_name, name])
-
-def walk_ast(tree, package=None):
-    importsx = []
-    path = "/Users/jwb/entwicklung/py3arch/tests/data"
+def walk_ast(tree, package=None, package_path=None):
+    modules = []
+    if package_path is None:
+        package_path = sys.path
     for node in ast.walk(tree):
         if isinstance(node, ast.Import):
-            importsx.extend([a.name for a in node.names])
+            modules.extend([a.name for a in node.names])
         elif isinstance(node, ast.ImportFrom):
             for alias in node.names:
-                name = resolve_import_from(alias.name, node.module, package=package, level=node.level,
-                    path=sys.path + [path])
-                importsx.append(name)
-    return importsx
+                fqname = resolve_import_from(alias.name, node.module, package=package, level=node.level)
+                # TODO(jwbargsten): check for builtins
+                fqname = resolve_module_or_object(fqname, path=package_path)
+                modules.append(fqname)
+    return modules
--- tests/test_import_finder.py
+++ tests/test_import_finder.py
@@ -1,36 +1,9 @@
 import ast
-import importlib
 import sys
-from py3arch.import_finder import walk_ast, find_module
-from importlib.util import find_spec
-
-# def find_module(self, name, path, parent=None):
-#     if parent is not None:
-#         # assert path is not None
-#         fullname = parent.__name__ + "." + name
-#     else:
-#         fullname = name
-#     if fullname in self.excludes:
-#         self.msgout(3, "find_module -> Excluded", fullname)
-#         raise ImportError(name)
-#
-#     if path is None:
-#         if name in sys.builtin_module_names:
-#             return (None, None, ("", "", _C_BUILTIN))
-#
-#         path = self.path
-#
-#     return _find_module(name, path)
-
-
-
-def test_find():
-    pass
-
+from py3arch.import_finder import walk_ast
 
 
 def test_ast_walk():
-    # subpkg1/subpkg1a/__init__.py
     data = """
 import datetime
 import pkgA
@@ -43,15 +16,6 @@ from ..subpkg2.subpkg2a import moduleM
 from ..subpkg2.moduleZ import CONSTANT_A
 """
     tree = ast.parse(data)
-    imports = walk_ast(tree, package="pkgA.subpkg1.subpkg1a")
+    imports = walk_ast(tree, package="pkgA.subpkg1.subpkg1a", package_path=sys.path + ["tests/data"])
     for imp in imports:
         print(imp)
-    # print(find_module(imp, ["/Users/jwb/entwicklung/py3arch/tests/data"] + sys.path))
-
-# from .module import h
-# from .moduleY import spam
-# from .moduleY import spam as ham
-# from . import moduleY
-# from ..subpackage1 import moduleY
-# from ..subpackage2.moduleZ import eggs
-# from ..moduleA import foo
