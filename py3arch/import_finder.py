ea4f8587bd9f8de3156b822efc8cffa840b85c55
Joachim Bargsten <jw@bargsten.org>
2022-11-21 07:48:54 +0100

    POC for transitive imports
    * added `_update_with_transitive_imports` to enrich the direct imports with transitive imports
    * added `is_circular` field to indicate circular imports
    * added `collect_from_pkg` as convenience method to analyze a complete pkg
    * started to set up config settings to make this module a pytest plugin
    * reverted back to file-based module vs. function detection
      (for checking if `b` is a function or module in `from a import b`)

--- py3arch/import_finder.py
+++ py3arch/import_finder.py
@@ -1,4 +1,3 @@
-import ast
 import sys
 import os
 from importlib.machinery import PathFinder
@@ -9,11 +8,8 @@ from importlib.machinery import PathFinder
 
 
 def resolve_module_or_object(fqname, path=None):
-    spec = find_spec(fqname, path)
-    return fqname if spec else fqname.rpartition(".")[0]
-
-
-def find_spec(fqname, path=None):
+    if path is None:
+        path = sys.path
     # taken from importlib.util.find_spec
     if fqname in sys.modules:
         module = sys.modules[fqname]
@@ -26,30 +22,29 @@ def find_spec(fqname, path=None):
         else:
             if spec is None:
                 raise ValueError("{}.__spec__ is None".format(fqname))
-            return spec
+            return fqname
 
     PathFinder.invalidate_caches()
 
     if "." not in fqname:
-        return PathFinder.find_spec(fqname, path)
+        return fqname
 
     parts = fqname.split(".")
-    while parts:
-        head = parts[0]
-        parts = parts[1:]
-        spec = PathFinder.find_spec(head, path)
-        if spec is None:
-            # if we cannot find the last part the "c" of "a.b.c"
-            # then we might try to import an object
-            # but if we already cannot find the spec for "a.b", then
-            # something is off
-            if len(parts) > 1:
-                raise ImportError("No module named {name!r}".format(name=head), name=head)
-            else:
-                return None
-        file_path = spec.origin
-        path = path + [os.path.dirname(file_path)]
-    return PathFinder.find_spec(head, path)
+    head = parts[0]
+
+    spec = PathFinder.find_spec(head, path)
+    if not hasattr(spec, "submodule_search_locations") or spec.submodule_search_locations is None:
+        # we imported an object, return "parent"
+        return ".".join(parts[:-1])
+
+    for base_path in spec.submodule_search_locations:
+        init = os.path.join(base_path, *(parts[1:] + ["__init__.py"]))
+        direct = os.path.join(base_path, *parts[1:]) + ".py"
+        if os.path.exists(init) or os.path.exists(direct):
+            # we have a module
+            return fqname
+    # we imported an object, return "parent"
+    return ".".join(parts[:-1])
 
 
 def resolve_import_from(name, module=None, package=None, level=None):
@@ -81,18 +76,3 @@ def explode_import(fqname):
     for p in parts[1:]:
         acc.append(".".join([acc[-1], p]))
     return acc
-
-
-def walk_ast(tree, package=None, path=None):
-    modules = []
-    if path is None:
-        path = sys.path
-    for node in ast.walk(tree):
-        if isinstance(node, ast.Import):
-            modules.extend([a.name for a in node.names])
-        elif isinstance(node, ast.ImportFrom):
-            for alias in node.names:
-                fqname = resolve_import_from(alias.name, node.module, package=package, level=node.level)
-                fqname = resolve_module_or_object(fqname, path=path)
-                modules.append(fqname)
-    return modules
