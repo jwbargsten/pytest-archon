07446ee2e3d07e7209e25427b8fa363c97e3c423
Arjan Molenaar <gaphor@gmail.com>
2022-11-23 12:05:12 +0100

    Add tests that use the fixture

---
+++ tests/test_pytest_fixture.py
@@ -0,0 +1,25 @@
+def test_rule_basic(archrule):
+    (
+        archrule("abc", "def")
+        .match("*collect")
+        .should_not_import("pytest_arch.import_finder")
+        .check("pytest_arch", path=["."])
+    )
+
+
+def test_rule_fail(pytester):
+    pytester.makepyfile(
+        """
+        import pytest_arch
+
+        def test_rule_fail(archrule):
+            (
+                archrule("abc", "def")
+                .match("*collect")
+                .should_not_import("importl*")
+                .check(pytest_arch)
+            )
+    """
+    )
+    result = pytester.runpytest()
+    result.assert_outcomes(failed=1)
