d98dd03558e0526724c171501ad11d4b4170ba12
Arjan Molenaar <gaphor@gmail.com>
2022-11-25 08:12:58 +0100

    Replace pkg_resources with importlib.resources
    importlib.resources is in the standard lib, pkg_resources is not.

--- pytest_arch/core_modules.py
+++ pytest_arch/core_modules.py
@@ -1,9 +1,7 @@
+import importlib.resources
 import sys
 from functools import lru_cache
-from pathlib import Path
 from typing import FrozenSet
-
-import pkg_resources
 
 
 @lru_cache()
@@ -13,14 +11,22 @@ def core_modules(version=None) -> FrozenSet[str]:
     if version >= (3, 10):
         modules = sys.stdlib_module_names | set(sys.builtin_module_names)
     else:
-        modules_file = Path(
-            pkg_resources.resource_filename(
-                "pytest_arch", str(Path("assets", "core-module-lists", f"{version[0]}.{version[1]}.txt"))
-            )
-        )
+        modules_file = _module_file_path(version)
         if not modules_file.exists():
             raise FileNotFoundError(
                 f"{modules_file} does not exist, perhaps your python version is not supported"
             )
         modules = frozenset(modules_file.read_text().splitlines())
     return modules
+
+
+def _module_file_path(version):
+    if sys.version_info < (3, 9):
+        with importlib.resources.path("pytest_arch", "assets") as p:
+            return p / "core-module-lists" / f"{version[0]}.{version[1]}.txt"
+    return (
+        importlib.resources.files("pytest_arch")
+        / "assets"
+        / "core-module-lists"
+        / f"{version[0]}.{version[1]}.txt"
+    )
